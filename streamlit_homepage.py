697ecfeba014ef3b034ac01f818a113bf56a79af
turanjade <ran.tu@mail.utoronto.ca>
2024-09-03 13:16:12 -0400

    change dictionary

--- streamlit_homepage.py
+++ streamlit_homepage.py
@@ -71,16 +71,16 @@ if page == "Home":
     st.markdown('Recent news')
 
     with st.container():
-    st.header("")
-    # Create columns in the top section
-    col1, col2 = st.columns(2)
-    with col1:
-        st.header("Taking a breath and a half-year break at UofT (Aug-16-2024)")
-        st.write("Starting from Aug 16, I will be visiting the Department of Chemical Engineering and Applied Chemistry at the University of Toronto. Hopefully, this visiting will bring much new ideas on the travel-related environmental and social impact and mitigation practices from a different perspective")
+        st.header("")
+        # Create columns in the top section
+        col1, col2 = st.columns(2)
+        with col1:
+            st.header("Taking a breath and a half-year break at UofT (Aug-16-2024)")
+            st.write("Starting from Aug 16, I will be visiting the Department of Chemical Engineering and Applied Chemistry at the University of Toronto. Hopefully, this visiting will bring much new ideas on the travel-related environmental and social impact and mitigation practices from a different perspective")
 
-    with col2:
-        st.header("An in-depth communication with Austria-based NGO IIASA (Jul-17-2024)")
-        st.write("Sponsored by the NSFC-funded international collaboration project, Decarbonization of Residents Life Behavior, we traveled to Vienna, Austria, and had a talk at the IIASA. MaaS business, travelers' behavior analysis, and influencing factors were discussed.")
+        with col2:
+            st.header("An in-depth communication with Austria-based NGO IIASA (Jul-17-2024)")
+            st.write("Sponsored by the NSFC-funded international collaboration project, Decarbonization of Residents Life Behavior, we traveled to Vienna, Austria, and had a talk at the IIASA. MaaS business, travelers' behavior analysis, and influencing factors were discussed.")
 
 
 
